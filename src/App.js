23bfee6a7d331801f7202162d8375c8af3b92aca
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2023-11-01 12:14:46 +0530

    firstPush

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Page/Main';
+function App() {
+  return (
+    <div >
+      <Main/>                  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Component/Header'
+import SideBar from '../Component/SideBar'
+
+const Main = () => {
+  return (
+    <div>
+        <Header/>
+        <SideBar/>
+        <div>Display mails</div>
+    </div>
+  )
+}
+
+export default Main
